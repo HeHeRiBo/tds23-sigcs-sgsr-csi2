e25d5f645c258af181905671631a5895fa46e984
Juan Arriagada <jarrcan@gmail.com>
2023-07-12 23:50:01 -0400

    💩 mock de módulo de stock

---
+++ app/stock/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+
+class Lote(models.Model):
+    pass
+
+
+class Consumo(models.Model):
+    pass
+
+
+class Stock(models.Model):
+    pass
+
+    def upd_cantidad(self):
+        pass
+
+    def upd_has_quiebre(self):
+        pass
+
+    def upd_caducidad(self):
+        pass
+
+
+class Movimiento(models.Model):
+    pass
