af910a46046b2b7fb75167451d44fc0d3b79e535
Juan Arriagada <jarrcan@gmail.com>
2023-07-15 22:51:56 -0400

    🧪 tests endpoints/views de stock y mock classes

---
+++ app/stock/views.py
@@ -0,0 +1,32 @@
+class MovimientoListCreateView:
+    pass
+
+
+class MovimientoRetrieveDestroyView:
+    pass
+
+
+class MovimientoLoteRetrieveView:
+    pass
+
+
+class MovimientoMedicamentoView:
+    def get():
+        pass
+
+
+class ConsumoListCreateView:
+    pass
+
+
+class ConsumoRetrieveDestroyVie:
+    pass
+
+
+class ConsumoMedicamentoView:
+    def get():
+        pass
+
+
+# class DisponibilidadMedicamentoView(views.APIView):
+#     pass
