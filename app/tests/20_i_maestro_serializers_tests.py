7184ac1d0745d1963c85045b9e6157d2988b9b14
Juan Arriagada <jarrcan@gmail.com>
2023-07-23 13:13:47 -0400

    🧪 tests adicionales para implementar

---
+++ app/tests/20_i_maestro_serializers_tests.py
@@ -0,0 +1,26 @@
+import pytest
+
+
+@pytest.mark.django_db
+def test_institucion_serializer():
+    pass
+
+
+@pytest.mark.django_db
+def test_medicamento_serializer():
+    pass
+
+
+@pytest.mark.django_db
+def test_item_serializer():
+    pass
+
+
+@pytest.mark.django_db
+def test_equipamiento_serializer():
+    pass
+
+
+@pytest.mark.django_db
+def test_quiebre_serializer():
+    pass
---
+++ app/tests/2_b_maestro_models_tests.py
@@ -0,0 +1,26 @@
+import pytest
+
+
+@pytest.mark.django_db
+def test_institucion_model():
+    pass
+
+
+@pytest.mark.django_db
+def test_medicamento_model():
+    pass
+
+
+@pytest.mark.django_db
+def test_item_model():
+    pass
+
+
+@pytest.mark.django_db
+def test_equipamiento_model():
+    pass
+
+
+@pytest.mark.django_db
+def test_quiebre_model():
+    pass
